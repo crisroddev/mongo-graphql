1c44880fa349a8ba9655cd31100c18ef7a02c74b
CrisRod <crodriguez85@gmail.com>
2019-09-06 16:24:59 -0400

    boilerplate index.js graphql

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 ```
 yarn add -D @babel/preset-env @babel/cli @babel/node @babel/core nodemon
 ```
+
+## ApolloServer
+```
+yarn add apollo-server-express express graphql
+```
--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('hi')
+import { ApolloServer } from 'apollo-server-express';
+import express from 'express';
+
+const app = express();
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+server.applyMiddleware({ app });
+
+app.listen( { port: 4000 }, () => 
+    console.log(`Server running at http://4000${server.graphqlPath}`)
+);
